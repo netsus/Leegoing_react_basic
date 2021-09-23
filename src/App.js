7eefc1b18f1d8883f0d1141e2258adf0f5ca4b50
netsus <cwjcsk@naver.com>
2021-09-23 16:49:55 +0900

    React - 11.1. 컴포넌트 만들기 1 리액트의 핵심 "컴포넌트"를 직접 만들기. 컴포넌트를 사용하여, 복잡한 웹사이트를 divide and conquer 하기위해 탄생한 기술

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Subject() {
+  return(
+    <header>
+      <h1>WEB</h1>
+      world wide web!
+    </header>
+  );
+
+}
+
 function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1>Hello React!!</h1>
+      <Subject></Subject>
     </div>
   );
 }
