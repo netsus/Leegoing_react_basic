1f6d4e988bdcdab4a195ae06a0d04b29686b03c8
netsus <cwjcsk@naver.com>
2021-09-23 16:59:03 +0900

    React - 11.2. 컴포넌트 만들기 2 App.js에서 TOC 컴포넌트와 Content 컴포넌트 생성 -> App 컴포넌트 내부에 사용자 정의 태그로 삽입 function App() {   return (     <div className="App">       <img src={logo} className="App-logo" alt="logo" />       <Subject></Subject>       <TOC></TOC>       <Content></Content>     </div>   ); }

--- src/App.js
+++ src/App.js
@@ -8,7 +8,27 @@ function Subject() {
       world wide web!
     </header>
   );
+}
 
+function TOC() {
+  return(
+    <nav>
+      <ul>
+        <li><a href="1.html">HTML</a></li>
+        <li><a href="2.html">CSS</a></li>
+        <li><a href="3.html">JavaScript</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+function Content() {
+  return(
+    <article>
+      <h2>HTML</h2>
+      HTML is HyperText Markup Language.
+    </article>
+  );
 }
 
 function App() {
@@ -16,6 +36,8 @@ function App() {
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
       <Subject></Subject>
+      <TOC></TOC>
+      <Content></Content>
     </div>
   );
 }
