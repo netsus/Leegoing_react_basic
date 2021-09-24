567b501b71413a1cfa55ca07a302eb4479e33042
netsus <cwjcsk@naver.com>
2021-09-24 20:30:32 +0900

    React - 14. Component 파일로 분리하기
    1. component/Subject.js : Subject 컴포넌트에 대해 함수형으로 컴포넌트 분리
    --> 컴포넌트 분리시 export default [컴포넌트 이름] 을 마지막에 작성해줘야한다.
      1) Subject.js 내용
    function Subject(props) {
      return(
        <header>
          <h1>{props.title}</h1>
          {props.sub}
        </header>
      );
    }
    export default Subject;
    
    2. App.js
      1) import 하기
      import Subject from "./components/Subject";
      2) App 컴포넌트에서 사용
    function App() {
      return (
        <div className="App">
          <Subject title="REACT" sub="For UI"></Subject>
        </div>
      );
    }

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 import logo from './logo.svg';
+import TOC from "./components/TOC"
+import Content from './components/Content';
+import Subject from './components/Subject';
 import './App.css';
-
-function Subject(props) {
-  return(
-    <header>
-      <h1>{props.title}</h1>
-      {props.sub}
-    </header>
-  );
-}
-
-function TOC() {
-  return(
-    <nav>
-      <ul>
-        <li><a href="1.html">HTML</a></li>
-        <li><a href="2.html">CSS</a></li>
-        <li><a href="3.html">JavaScript</a></li>
-      </ul>
-    </nav>
-  );
-}
-
-function Content(props) {
-  return(
-    <article>
-      <h2>{props.title}</h2>
-      {props.desc}
-    </article>
-  );
-}
 
 function App() {
   return (
