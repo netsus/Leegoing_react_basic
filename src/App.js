cb0ea29ea339ef48bc6a067df21919b68afedf79
netsus <cwjcsk@naver.com>
2021-09-24 20:50:43 +0900

    React - 15.1. State 소개 Function에서 state 정의하기 1. App 컴포넌트 내부에서 state 정의   const state = {     subject:{title:"REACT", sub:"For UI"}
    2. state 사용 -> props로 하우 컴포넌트에 전달
    <Subject
        title={state.subject.title}
        sub={state.subject.sub}>
     </Subject>

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import TOC from "./components/TOC"
 import Content from './components/Content';
@@ -5,14 +6,36 @@ import Subject from './components/Subject';
 import './App.css';
 
 function App() {
+  const state = {
+    subject:{title:"REACT", sub:"For UI"}
+  }
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
-      <Subject title="REACT" sub="For UI"></Subject>
+      <Subject 
+        title={state.subject.title}
+        sub={state.subject.sub}>
+       </Subject>
       <TOC></TOC>
       <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
     </div>
   );
 }
 
+
+// class App extends Component {
+//   constructor(props){
+//     super(props);
+//   }
+//   render(){
+//     return (
+//       <div className="App">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <Subject title="REACT" sub="For UI"></Subject>
+//         <TOC></TOC>
+//         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+//       </div>
+//     );
+//     }
+//   }
 export default App;
