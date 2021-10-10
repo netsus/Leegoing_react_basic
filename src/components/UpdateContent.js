d8e0e631d1fdc969d23b7cfb1a34faa1a8aeae20
netsus <cwjcsk@naver.com>
2021-10-11 00:40:00 +0100

    React - 20.2. update 구현 : form App.js에서 data로 전달받은 props를 form에 입력시키고, 수정할 수 있게 기능을 구현
    1. useState 사용해서, title, desc State 생성
      const [title, setTitle] = useState(props.data.title);
      const [desc, setDesc] = useState(props.data.desc);
    
    2. form태그 하위의 title input 태그 변경
      1) value 프로퍼티에 {title} 부여 -> value={title}
      2) onChange에 setTitle 함수 부여 -> onChange={function(e){ setTitle(e.target.value);  }} => 기존값을 바꿨을 때, value에 바뀐값을 넣음으로써 우리가 수정하는 것처럼 만듦.
    
    2. textarea태그에 desc 적용
      1) 위와 마찬가지로 value 태그에 desc 부여 -> value={desc}
      2) 위와 마찬가지로 onChange프로퍼티에 setDesc 함수 부여
    
    onChange 프로퍼티 작동 원리 : 변화가 있을때마다 실행 -> value에 들어가는 변수의 값을 e.target.value로 변경함으로써 내가 수정하는 대로 해당 변수가 수정되고 그게 value로 들어감.

--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -1,5 +1,9 @@
+import React, { useState } from 'react';
 
 function UpdateContent(props) {
+  const [title, setTitle] = useState(props.data.title);
+  const [desc, setDesc] = useState(props.data.desc);
+
   console.log('UpdateContent render');
   console.log(props.data);
   return(
@@ -15,12 +19,26 @@ function UpdateContent(props) {
          alert('Submit!!!')
        }}
       >
-        <p><input type="text" name="title" placeholder="title" /></p>
         <p>
-          <textarea name="desc" placeholder="description"></textarea>
+          <input 
+            type="text" 
+            name="title" 
+            placeholder="title"
+            value={title}
+            onChange={function(e){
+              setTitle(e.target.value);
+            }} /></p>
+        <p>
+          <textarea 
+            name="desc" 
+            placeholder="description"
+            value={desc}
+            onChange={function(e){
+              setDesc(e.target.value);
+            }}></textarea>
         </p>
         <p>
-          <input type="submit" value="Submit" />
+          <input type="submit" />
         </p>
       </form>
     </article>
